87a157e0b78349ef479e00cd99b8a9cfe71cd55d
Gabriel Aguiar <fgsoftwarestudio@gmail.com>
2022-11-04 19:56:12 +0000

    add math lib
    Signed-off-by: Gabriel Aguiar <fgsoftwarestudio@gmail.com>

--- src/test.cpp
+++ src/test.cpp
@@ -1,4 +1,14 @@
+#include "include/math/Vector3.hpp"
 
 int main(int argc, const char * argv[]){
-    Vector3 v(0,1,0);
+    fgengine::math::Vector3 v(0,1,0);
+    fgengine::math::Vector3 axis(1,0,0);
+
+	fgengine::math::Vector3 rotatedVector=v.rotateVectorAboutAngleAndAxis(90,axis);
+
+	v.show();
+	axis.show();
+	rotatedVector.show();
+
+    return 0;
 }
