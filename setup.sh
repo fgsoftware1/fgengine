acf38ba93412dd1cbf77605a4aa2d0157faaeaf7
ᛒᛅᚱᚾᚢᛚᚠ <60277618+fgsoftware1@users.noreply.github.com>
2023-08-22 19:00:19 +0100

    updating vscode settings
    Signed-off-by: ᛒᛅᚱᚾᚢᛚᚠ <60277618+fgsoftware1@users.noreply.github.com>

--- setup.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-apt install -y cmake pkg-config mesa-utils freeglut3-dev mesa-common-dev python3 python3-glad libopengl-dev libglfw3-dev libspdlog-dev
-glad --profile compatibility --out-path vendor/glad --generator c
